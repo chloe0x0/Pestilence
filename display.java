911d91781d4689643f17c0a7f6be547c72dc0922
ChloeZeiter <84647960+ChloeZeiter@users.noreply.github.com>
2021-05-24 17:25:48 -0700

    Updated display to handle new states
    Will later add custom colors and more GUI features

--- display.java
+++ display.java
@@ -10,42 +10,53 @@ import java.awt.event.*;
 
 import java.util.Scanner;
 
+import java.io.IOException;
+
 class DisplayPanel extends JPanel implements KeyListener{
-    private int[][] state_space;
     private Boolean isLooping;
-    private engine e;
+    engine e;
     public DisplayPanel(int width, int height){
         this.setBackground(Color.BLACK);
-        this.e = new engine(width, height, "moore");
+        this.e = new engine(width, height, "moore", true);
         this.isLooping = false;
         addKeyListener(this);
         this.setFocusable(true);
     }
 
-    public void engine_init(double recovery_rate, double inf_period, double attack_rate, double infection_seed){
-        e.engineInit(recovery_rate, inf_period, attack_rate, infection_seed);
+    public void engine_init(double R0, double fatality, int incubation, int infectious, double immunity_gain, double infection_seed){
+        e.engineInit(R0, fatality, incubation, infectious, immunity_gain, infection_seed);
     }
 
     @Override
     public void paintComponent(Graphics g){
         super.paintComponent(g);
-        int[][] lattice = e.getLattice();
+        engine.cell[][] lattice = e.getLattice();
         for (int x = 0; x < lattice.length; ++x){
             for (int y = 0; y < lattice[0].length; ++y){
-                if (lattice[x][y] == 0){
+                if (lattice[x][y].state == 0){
+                    g.setColor(Color.GRAY);
+                    g.fillRect(x, y, 1, 1);
+                }
+                else if (lattice[x][y].state == 1){
                     g.setColor(Color.GREEN);
                     g.fillRect(x, y, 1, 1);
                 }
-                else if (lattice[x][y] == 1){
+                else if (lattice[x][y].state == 2){
+                    g.setColor(Color.YELLOW);
+                    g.fillRect(x, y, 1, 1);
+                }
+                else if (lattice[x][y].state == 3){
                     g.setColor(Color.RED);
                     g.fillRect(x, y, 1, 1);
                 }
-                else if (lattice[x][y] == 2){
+                else if (lattice[x][y].state == 4){
                     g.setColor(Color.BLUE);
                     g.fillRect(x, y, 1, 1);
                 }
             }
         }
+        g.setColor(Color.BLACK);
+        g.drawString(Integer.toString(e.getIteration()), 150, 150);
     }
 
     public void epoch(){
@@ -86,33 +97,43 @@ class Frame extends JFrame{
 }
 
 public class display{
-    public static void main(String[] args) throws InterruptedException{
+    public static void main(String[] args) throws InterruptedException, IOException{
         Scanner sc = new Scanner(System.in);
-        double recovery_rate, inf_period, attack_rate, infection_seed;
-        int width, height;
+
+        double R0, fatality, immunity_gain, infection_seed;
+        int incubation, infectious, width, height;
+
 
         System.out.print("Width of lattice: ");
-        width = sc.nextInt();
+        width = sc.nextInt(); 
 
-        System.out.print("Height of lattice: ");
+        System.out.print("Height of the lattice: ");
         height = sc.nextInt();
 
-        System.out.print("Recovery Rate: ");
-        recovery_rate = sc.nextDouble();
+        System.out.print("R0: ");
+        R0 = sc.nextDouble();
+
+        System.out.print("fatality: ");
+        fatality = sc.nextDouble();
+
+        System.out.print("incubation: ");
+        incubation = sc.nextInt();
+
+        System.out.print("infectious: ");
+        infectious = sc.nextInt();
+
+        System.out.print("immunity_gain: ");
+        immunity_gain = sc.nextDouble();
         
-        System.out.print("Infectious Period: ");
-        inf_period = sc.nextDouble();
-
-        System.out.print("Attack Rate: ");
-        attack_rate = sc.nextDouble();
-
-        System.out.print("Infection Seed Probability: ");
+        System.out.print("infection seed probability: ");
         infection_seed = sc.nextDouble();
 
-        
         Frame f = new Frame(width, height);
-        f.p.engine_init(recovery_rate, inf_period, attack_rate, infection_seed);
+        f.p.engine_init(R0, fatality, incubation, infectious, immunity_gain, infection_seed);
         while(f.p.isVisible()){
+            if (f.p.e.getIteration() == 15){
+                f.p.e.extractToCSV("dataTest.csv");
+            }
             f.p.epoch();
             f.p.repaint();
             Thread.sleep(10);
