dfc3b83762955c58f95323b0486447c013577b4f
ChloeZeiter <84647960+ChloeZeiter@users.noreply.github.com>
2021-06-04 16:04:02 -0500

    Small Python script to visualize CSV data

---
+++ DataGraphing.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from typing import List
+
+def readCSV(filename : str, features : List[str])->object:
+    df = pd.read_csv("dataTest.csv")
+    df = df[features]
+    return df
+
+getFeatures = lambda inputStr : inputStr.split(" ")
+
+#def analyzeStatistics(dataFrame : object):
+#    print(dataFrame.describe())
+
+
+if __name__ == "__main__":
+    fileName = input("Enter path of CSV ")
+    features = getFeatures(input("Enter statistics to plot "))
+
+    try:
+        df = readCSV(fileName, features)
+    except KeyError:
+        print(f"invalid statistic identifier in features: {features}")
+
+    df.plot()
+    plt.show()
+
+#    analyzeStatistics(df)
